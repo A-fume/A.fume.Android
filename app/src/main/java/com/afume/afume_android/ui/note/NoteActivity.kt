09e30d2808ce719b8057111fc6940506f03952fa
Haeeul <etoilune00@gmail.com>
2020-12-13 07:06:04 +0900

    [feat] #21 Add Note View

---
+++ app/src/main/java/com/afume/afume_android/ui/note/NoteActivity.kt
@@ -0,0 +1,12 @@
+package com.afume.afume_android.ui.note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.afume.afume_android.R
+
+class NoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note)
+    }
+}
