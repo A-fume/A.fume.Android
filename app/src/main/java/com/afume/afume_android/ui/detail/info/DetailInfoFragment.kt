9232fe4164e24bf5976ea0a4eacf3540f0791f0a
EuneeChung <chungee375@gmail.com>
2020-12-18 04:14:53 +0900

    [feat] #25 Add overall outline about details info tab

--- app/src/main/java/com/afume/afume_android/ui/detail/info/DetailInfoFragment.kt
+++ app/src/main/java/com/afume/afume_android/ui/detail/info/DetailInfoFragment.kt
@@ -1,5 +1,6 @@
 package com.afume.afume_android.ui.detail.info
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,19 +8,93 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.afume.afume_android.R
+import com.afume.afume_android.data.vo.HomePerfumeListData
 import com.afume.afume_android.databinding.FragmentDetailInfoBinding
 import com.afume.afume_android.databinding.FragmentDetailNoteBinding
+import com.afume.afume_android.ui.filter.FlexboxRecyclerViewAdapter
+import com.afume.afume_android.ui.filter.RvFlexboxData
+import com.afume.afume_android.ui.home.adapter.PopularListAdapter
+import com.google.android.flexbox.AlignItems
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
 
 class DetailInfoFragment : Fragment() {
 
     lateinit var binding: FragmentDetailInfoBinding
+    lateinit var rvKeywordAdapter: FlexboxRecyclerViewAdapter
+    lateinit var rvSimilarAdapter: PopularListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail_info, container, false)
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRvKeyword(context)
+        initRvSimilar(context!!)
+    }
+
+    private fun initRvKeyword(ctx:Context?) {
+        val flexboxLayoutManager = FlexboxLayoutManager(ctx).apply {
+            flexDirection = FlexDirection.ROW
+            flexWrap = FlexWrap.WRAP
+            alignItems = AlignItems.STRETCH
+        }
+
+        rvKeywordAdapter = FlexboxRecyclerViewAdapter(2)
+        binding.rvDetailsInfoKeyword.apply {
+            adapter = rvKeywordAdapter
+            layoutManager = flexboxLayoutManager
+        }
+
+        rvKeywordAdapter.data = listOf(
+            RvFlexboxData("#꽃향의"),
+            RvFlexboxData("#여성스러운"),
+            RvFlexboxData("#데일리"),
+            RvFlexboxData("#여대생"),
+            RvFlexboxData("#러블리한"),
+            RvFlexboxData("#세련된"),
+            RvFlexboxData("#봄같은"),
+            RvFlexboxData("#첫사랑같은"),
+            RvFlexboxData("#소녀스러운")
+        )
+        rvKeywordAdapter.notifyDataSetChanged()
+    }
+    private fun initRvSimilar(ctx: Context){
+        rvSimilarAdapter= PopularListAdapter(ctx)
+        binding.rvDetailsInfoSimilar.adapter=rvSimilarAdapter
+        rvSimilarAdapter.data=mutableListOf(
+            HomePerfumeListData(
+                image = null,
+                brand = "1번 브랜드",
+                name = "1번향수",
+                like = 0
+            ),
+            HomePerfumeListData(
+                image = null,
+                brand = "2번 브랜드",
+                name = "2번향수",
+                like = 1
+            ),
+            HomePerfumeListData(
+                image = null,
+                brand = "3번 브랜드",
+                name = "3번향수",
+                like = 1
+            ),
+            HomePerfumeListData(
+                image = null,
+                brand = "4번 브랜드",
+                name = "4번향수",
+                like = 0
+            )
+        )
+        rvSimilarAdapter.notifyDataSetChanged()
     }
 }
