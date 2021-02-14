68b3f78c8607a64f4bfaaa8249dc616c6a918860
Haeeul <etoilune00@gmail.com>
2021-02-15 03:12:33 +0900

    [refactor] #32 Modified nickname form at signUpViewModel

--- app/src/main/java/com/afume/afume_android/ui/signup/SignUpEmailActivity.kt
+++ app/src/main/java/com/afume/afume_android/ui/signup/SignUpEmailActivity.kt
@@ -3,19 +3,17 @@ package com.afume.afume_android.ui.signup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.afume.afume_android.R
 import com.afume.afume_android.databinding.ActivitySignUpEmailBinding
-import com.afume.afume_android.util.CheckTextWatcher
 
 class SignUpEmailActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpEmailBinding
     private val signUpViewModel : SignUpViewModel by viewModels()
-    var checkAni = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,50 +21,40 @@ class SignUpEmailActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         binding.viewModel = signUpViewModel
 
-        checkNicknameInput()
-
         binding.edtSignUpEmail.requestFocus()
 
-    }
-
-    private fun checkNicknameInput(){
-        binding.imgSignUpCheckNick.visibility = View.INVISIBLE
-
-        binding.edtSignUpNick.addTextChangedListener(CheckTextWatcher{checkNicknameForm()})
-    }
-
-    private fun checkNicknameForm() {
-//        android.util.Patterns.matches("^[a-zA-Z0-9가-힣]*$",binding.edtSignUpNick
-
-        if (binding.edtSignUpNick.text.isNotEmpty()) {
-            binding.imgSignUpCheckNick.visibility = View.VISIBLE
-        } else {
-            binding.imgSignUpCheckNick.visibility = View.INVISIBLE
-        }
-
+        startAnimation()
         checkNextButton()
     }
 
-    private fun checkNextButton(){
-        binding.clSignUpEmailNext.visibility = View.INVISIBLE
+    private fun startAnimation(){
+        signUpViewModel.nick.observe(this, Observer { isValidNick->
+            isValidNick?.let {
+                if(isValidNick){
+                    val animation = AnimationUtils.loadAnimation(this, R.anim.alpha_up)
+                    binding.clSignUpNick.startAnimation(animation)
 
-        if(binding.imgSignUpCheckEmail.visibility == View.VISIBLE && binding.imgSignUpCheckNick.visibility == View.VISIBLE){
-            binding.clSignUpEmailNext.visibility = View.VISIBLE
-        }else{
-            binding.clSignUpEmailNext.visibility = View.INVISIBLE
-        }
+                    binding.edtSignUpNick.requestFocus()
+                }
+            }
+        })
     }
 
-    private fun startAnimation(){
-        if(checkAni){
-            checkAni = false
-            binding.clSignUpNick.visibility = View.VISIBLE
-
-            val animation = AnimationUtils.loadAnimation(this,R.anim.alpha_up)
-            binding.clSignUpNick.startAnimation(animation)
-
-            binding.edtSignUpNick.requestFocus()
-        }
+    private fun checkNextButton(){
+        signUpViewModel.isValidEmailImage.observe(this, {isValidEmailImage->
+            isValidEmailImage?.let {
+                if(isValidEmailImage){
+                    signUpViewModel.checkNextBtn()
+                }
+            }
+        })
+        signUpViewModel.isValidNickImage.observe(this, {isValidNickImage->
+            isValidNickImage?.let {
+                if(isValidNickImage){
+                    signUpViewModel.checkNextBtn()
+                }
+            }
+        })
     }
 
     fun onClickNextBtn(view: View){
