c97750e274e0e8e06e1e6348ce3611b6fd922c38
Haeeul <etoilune00@gmail.com>
2021-03-23 23:49:47 +0900

    [feat] #26 Add my info edit view

---
+++ app/src/main/java/com/afume/afume_android/ui/setting/MyInfoEditActivity.kt
@@ -0,0 +1,12 @@
+package com.afume.afume_android.ui.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.afume.afume_android.R
+
+class MyInfoEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_info_edit)
+    }
+}
