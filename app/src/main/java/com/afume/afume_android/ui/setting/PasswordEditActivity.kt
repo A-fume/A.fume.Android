543219ca44eea187e9a0d66e8771787fa4d0e0fa
Haeeul <etoilune00@gmail.com>
2021-03-25 23:44:40 +0900

    [feat] #26 Add password edit view

---
+++ app/src/main/java/com/afume/afume_android/ui/setting/PasswordEditActivity.kt
@@ -0,0 +1,12 @@
+package com.afume.afume_android.ui.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.afume.afume_android.R
+
+class PasswordEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_edit)
+    }
+}
