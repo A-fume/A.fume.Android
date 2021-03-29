839d39e258681ba20e8cfb07efc331834eaadd21
Haeeul <etoilune00@gmail.com>
2021-03-29 19:57:39 +0900

    [refactor] #32 Modify gender icon based on binding

--- app/src/main/java/com/afume/afume_android/ui/signup/SignUpGenderActivity.kt
+++ app/src/main/java/com/afume/afume_android/ui/signup/SignUpGenderActivity.kt
@@ -5,10 +5,8 @@ import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import com.afume.afume_android.R
 import com.afume.afume_android.databinding.ActivitySignUpGenderBinding
-import com.afume.afume_android.util.setSelectedGenderTxt
 import com.afume.afume_android.util.startActivity
 
 class SignUpGenderActivity : AppCompatActivity() {
@@ -21,26 +19,6 @@ class SignUpGenderActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         binding.viewModel = signUpViewModel
 
-        setSelectedFont()
-    }
-
-    private fun setSelectedFont(){
-        signUpViewModel.isCheckMan.observe(this, Observer { isCheckMan ->
-            isCheckMan?.let{
-                if(isCheckMan){
-                    binding.txtSignUpGenderMan.setSelectedGenderTxt(true)
-                    binding.txtSignUpGenderWoman.setSelectedGenderTxt(false)
-                }
-            }
-        })
-        signUpViewModel.isCheckWoman.observe(this, Observer { isCheckWoman ->
-            isCheckWoman?.let{
-                if(isCheckWoman){
-                    binding.txtSignUpGenderMan.setSelectedGenderTxt(false)
-                    binding.txtSignUpGenderWoman.setSelectedGenderTxt(true)
-                }
-            }
-        })
     }
 
     fun onClickNextBtn(view: View){
