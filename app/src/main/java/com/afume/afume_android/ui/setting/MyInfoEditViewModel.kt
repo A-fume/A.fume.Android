088cf779393f9a9385485188e859315326abf164
Haeeul <etoilune00@gmail.com>
2021-03-30 18:29:33 +0900

    [feat] #26 Add edit password ViewModel

--- app/src/main/java/com/afume/afume_android/ui/setting/MyInfoEditViewModel.kt
+++ app/src/main/java/com/afume/afume_android/ui/setting/MyInfoEditViewModel.kt
@@ -11,9 +11,13 @@ class MyInfoEditViewModel : ViewModel() {
     // 입력 내용
     val nickTxt = MutableLiveData<String>("")
     val ageTxt = MutableLiveData<String>("")
+    val passwordTxt = MutableLiveData<String>("")
+    val newPasswordTxt = MutableLiveData<String>("")
+    val againPasswordTxt = MutableLiveData<String>("")
 
     // 안내문 내용
     val nickNotice = MutableLiveData<String>()
+    val passwordNotice = MutableLiveData<String>()
 
     // 자동입력
     fun checkMyInfo(){
@@ -126,5 +130,142 @@ class MyInfoEditViewModel : ViewModel() {
     fun onClickWomanBtn(){
         _isCheckMan.postValue(false)
         _isCheckWoman.postValue(true)
+    }
+
+    // 본인확인 검사 - 하단 안내문
+    private val _isValidPasswordNotice = MutableLiveData<Boolean>(false)
+    val isValidPasswordNotice : LiveData<Boolean>
+        get() = _isValidPasswordNotice
+
+    // 본인확인 버튼 노출 여부
+    private val _isValidPasswordBtn = MutableLiveData<Boolean>(false)
+    val isValidPasswordBtn : LiveData<Boolean>
+        get() = _isValidPasswordBtn
+
+    // 본인확인
+    private val _isValidPassword = MutableLiveData<Boolean>(false)
+    val isValidPassword : LiveData<Boolean>
+        get() = _isValidPassword
+
+    // 새비밀번호 입력란 노출
+    private val _newPasswordForm = MutableLiveData<Boolean>(false)
+    val newPasswordForm : LiveData<Boolean>
+        get() = _newPasswordForm
+
+    // 비밀번호 입력 실시간 확인
+    fun inputPassword(s: CharSequence?, start: Int, before: Int, count: Int) {
+        Handler().postDelayed({ checkPasswordForm() }, 0L)
+
+        resetValidPassword()
+    }
+
+    private fun resetValidPassword(){
+        if(_isValidPassword.value!!){
+            _isValidPassword.postValue(false)
+            _isValidPasswordBtn.postValue(true)
+        }
+    }
+
+    // 비밀번호 자리수 확인
+    private fun checkPasswordForm() {
+        passwordNotice.value = "4자리 이상 입력해주세요."
+        when(passwordTxt.value?.length){
+            0 -> _isValidPasswordNotice.postValue(false)
+            in 1..3 -> {
+                _isValidPasswordNotice.postValue(true)
+            }
+            else -> {
+                _isValidPasswordNotice.postValue(false)
+                _isValidPasswordBtn.postValue(true)
+            }
+        }
+    }
+
+    fun checkValidPassword(){
+        passwordNotice.value = "비밀번호를 다시 입력해주세요."
+        if(passwordTxt.value == AfumeApplication.prefManager.userPassword){
+            _isValidPassword.postValue(true)
+            _isValidPasswordBtn.postValue(false)
+            _isValidPasswordNotice.postValue(false)
+
+            if(!_newPasswordForm.value!!) _newPasswordForm.postValue(true)
+        }else{
+            _isValidPassword.postValue(false)
+            _isValidPasswordBtn.postValue(true)
+            _isValidPasswordNotice.postValue(true)
+        }
+    }
+
+    // 새비밀번호 형식 검사 - 하단 안내문
+    private val _isValidNewPasswordNotice = MutableLiveData<Boolean>(false)
+    val isValidNewPasswordNotice : LiveData<Boolean>
+        get() = _isValidNewPasswordNotice
+
+    // 새비밀번호 형식 검사 - 우측 이미지
+    private val _isValidNewPassword = MutableLiveData<Boolean>(false)
+    val isValidNewPassword : LiveData<Boolean>
+        get() = _isValidNewPassword
+
+
+    // 새비밀번호 실시간 확인
+    fun inputNewPassword(s: CharSequence?, start: Int, before: Int, count: Int) {
+        Handler().postDelayed({ checkNewPasswordForm() }, 1000L)
+    }
+
+    // 새비밀번호 자리수 확인
+    private fun checkNewPasswordForm(){
+        when(newPasswordTxt.value?.length){
+            0 -> _isValidNewPasswordNotice.postValue(false)
+            in 1..3 -> {
+                _isValidNewPasswordNotice.postValue(true)
+                _isValidNewPassword.postValue(false)
+            }
+            else -> {
+                _isValidNewPasswordNotice.postValue(false)
+                _isValidNewPassword.postValue(true)
+            }
+        }
+        checkAgainForm()
+    }
+
+    // 새비밀번호 일치 검사 - 하단 안내문
+    private val _isValidAgainPasswordNotice = MutableLiveData<Boolean>(false)
+    val isValidAgainPasswordNotice : LiveData<Boolean>
+        get() = _isValidAgainPasswordNotice
+
+    // 새비밀번호 일치 검사 - 우측 이미지
+    private val _isValidAgainPassword = MutableLiveData<Boolean>(false)
+    val isValidAgainPassword : LiveData<Boolean>
+        get() = _isValidAgainPassword
+
+    // 새비밀번호 일치 실시간 확인
+    fun inputAgainPassword(s: CharSequence?, start: Int, before: Int, count: Int) {
+        Handler().postDelayed({ checkAgainForm() }, 1000L)
+    }
+
+    private fun checkAgainForm(){
+        when{
+            newPasswordTxt.value.toString() == againPasswordTxt.value.toString() -> {
+                _isValidAgainPasswordNotice.postValue(false)
+                _isValidAgainPassword.postValue(true)
+            }
+            againPasswordTxt.value?.length == 0 -> {
+                _isValidAgainPasswordNotice.postValue(false)
+            }
+            else -> {
+                _isValidAgainPasswordNotice.postValue(true)
+                _isValidAgainPassword.postValue(false)
+            }
+        }
+    }
+
+    // 다음 버튼 노출 - 비밀번호
+    private val _passwordEditCompleteBtn = MutableLiveData<Boolean>(false)
+    val passwordEditCompleteBtn : LiveData<Boolean>
+        get() = _passwordEditCompleteBtn
+
+    // 다음 버튼 노출 여부 검사 - 비밀번호
+    fun checkPasswordNextBtn(){
+        _passwordEditCompleteBtn.postValue(_isValidNewPassword.value == true && _isValidAgainPassword.value == true)
     }
 }
--- app/src/main/java/com/afume/afume_android/ui/setting/PasswordEditActivity.kt
+++ app/src/main/java/com/afume/afume_android/ui/setting/PasswordEditActivity.kt
@@ -1,12 +1,64 @@
 package com.afume.afume_android.ui.setting
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.afume.afume_android.R
+import com.afume.afume_android.databinding.ActivityPasswordEditBinding
+import com.afume.afume_android.util.closeKeyboard
 
 class PasswordEditActivity : AppCompatActivity() {
+    lateinit var binding : ActivityPasswordEditBinding
+    private val editViewModel : MyInfoEditViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_password_edit)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_password_edit)
+        binding.lifecycleOwner = this
+        binding.viewModel = editViewModel
+
+        binding.edtEditPasswordCheck.requestFocus()
+
+        passwordAnimation()
+        checkNextBtn()
+        setKeyboard()
+    }
+
+    private fun passwordAnimation(){
+        editViewModel.newPasswordForm.observe(this, Observer { isValidPassword ->
+            isValidPassword?.let {
+                if(isValidPassword){
+                    val animation = AnimationUtils.loadAnimation(this, R.anim.alpha_up)
+                    binding.clPasswordEditNew.startAnimation(animation)
+
+                    binding.clPasswordEditNew.requestFocus()
+                }
+            }
+        })
+    }
+
+    private fun checkNextBtn(){
+        editViewModel.isValidNewPassword.observe(this, Observer{
+            editViewModel.checkPasswordNextBtn()
+        })
+        editViewModel.isValidAgainPassword.observe(this, Observer{
+            editViewModel.checkPasswordNextBtn()
+        })
+    }
+
+    private fun setKeyboard(){
+        editViewModel.passwordEditCompleteBtn.observe(this, Observer { passwordEditCompleteBtn ->
+            if(passwordEditCompleteBtn){
+                this.closeKeyboard()
+            }
+        })
+    }
+
+    fun onClickBackBtn(view: View){
+        finish()
     }
 }
