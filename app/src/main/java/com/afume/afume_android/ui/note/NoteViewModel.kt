03cb0e21c713553cfc48ed80ec695c76576aac0f
Haeeul <etoilune00@gmail.com>
2021-05-06 23:48:54 +0900

    [refactor] #57 Modify complete button at note view

--- app/src/main/java/com/afume/afume_android/ui/note/NoteViewModel.kt
+++ app/src/main/java/com/afume/afume_android/ui/note/NoteViewModel.kt
@@ -1,5 +1,6 @@
 package com.afume.afume_android.ui.note
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -7,5 +8,10 @@ class NoteViewModel : ViewModel() {
 
     // 입력 내용
     val contentsTxt = MutableLiveData<String>("")
+
+    // 완료 버튼 활성화
+    private val _completeBtn = MutableLiveData<Boolean>(false)
+    val completeBtn : LiveData<Boolean>
+        get() = _completeBtn
 
 }
