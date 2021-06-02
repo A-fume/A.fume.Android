6daedfffb6aad155ea96571c958bda3dfbf02465
EuneeChung <chungee375@gmail.com>
2021-06-03 01:20:28 +0900

    [feature] #45 Set view details about navigation drawer

--- app/src/main/java/com/afume/afume_android/ui/my/MyFragment.kt
+++ app/src/main/java/com/afume/afume_android/ui/my/MyFragment.kt
@@ -1,5 +1,6 @@
 package com.afume.afume_android.ui.my
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,8 @@ import com.afume.afume_android.databinding.FragmentMypageBinding
 import com.afume.afume_android.ui.filter.AfumeViewPagerAdapter
 import com.afume.afume_android.ui.my.myperfume.MyPerfumeFragment
 import com.afume.afume_android.ui.my.wishlist.WishListFragment
+import com.afume.afume_android.ui.setting.EditMyInfoActivity
+import com.afume.afume_android.ui.setting.EditPasswordActivity
 import com.afume.afume_android.util.TabSelectedListener
 
 class MyFragment : Fragment() {
@@ -23,7 +26,7 @@ class MyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMypageBinding.inflate(inflater, container, false)
-        binding.lifecycleOwner=this
+        binding.lifecycleOwner = this
         return binding.root
     }
 
@@ -34,48 +37,69 @@ class MyFragment : Fragment() {
         setUpTabWithViewPager()
         setNavigation()
     }
-    private fun initBind(){
-        
-        binding.toolbarMypage.toolbartxt="마이"
-        //바궈야함
-        binding.toolbarMypage.toolbar=R.drawable.icon_btn_sidebar
+
+    override fun onResume() {
+        super.onResume()
+        binding.drawerLayout.closeDrawers()
 
     }
 
-    private fun initVp(){
-        myPagePagerAdapter= AfumeViewPagerAdapter(childFragmentManager)
-        myPagePagerAdapter.fragments= listOf(
+    private fun initBind() {
+
+        binding.toolbarMypage.toolbartxt = "마이"
+        //바궈야함
+        binding.toolbarMypage.toolbar = R.drawable.icon_btn_sidebar
+
+    }
+
+    private fun initVp() {
+        myPagePagerAdapter = AfumeViewPagerAdapter(childFragmentManager)
+        myPagePagerAdapter.fragments = listOf(
             MyPerfumeFragment(),
             WishListFragment()
         )
-        binding.vpMypage.adapter=myPagePagerAdapter
+        binding.vpMypage.adapter = myPagePagerAdapter
 
     }
 
-    private fun setUpTabWithViewPager(){
+    private fun setUpTabWithViewPager() {
         binding.tabMypage.setupWithViewPager(binding.vpMypage)
         binding.tabMypage.apply {
-            getTabAt(0)?.text="마이"
-            getTabAt(1)?.text="위시 리스트"
+            getTabAt(0)?.text = "마이"
+            getTabAt(1)?.text = "위시 리스트"
         }
         binding.tabMypage.addOnTabSelectedListener(TabSelectedListener(binding.tabMypage))
     }
-    private fun setNavigation(){
+
+    private fun setNavigation() {
         binding.toolbarMypage.toolbarBtn.setOnClickListener {
             binding.drawerLayout.openDrawer(binding.myNavigationDrawer)
         }
-        binding.myNavigationDrawer.setNavigationItemSelectedListener {menuItem->
-            menuItem.isChecked=true
-            binding.drawerLayout.closeDrawer(binding.drawerLayout)
+
+        binding.myNavigationDrawer.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+
+            when (menuItem.itemId) {
+                R.id.edit_my_info -> intent(EditMyInfoActivity::class.java)
+                R.id.edit_password -> intent(EditPasswordActivity::class.java)
+                R.id.logout -> {}
+            }
+//            binding.drawerLayout.closeDrawer(binding.drawerLayout)
             true
         }
 
-        binding.myNavigationDrawer.getHeaderView(0).findViewById<ImageView>(R.id.btn_cancle).setOnClickListener {
-            binding.drawerLayout.closeDrawers()
-        }
+
+        binding.myNavigationDrawer.getHeaderView(0).findViewById<ImageView>(R.id.btn_cancle)
+            .setOnClickListener {
+                binding.drawerLayout.closeDrawers()
+            }
+
     }
 
-
+    fun intent(activity: Class<*>) {
+        val intent = Intent(requireContext(), activity)
+        startActivity(intent)
+    }
 
 
 }
