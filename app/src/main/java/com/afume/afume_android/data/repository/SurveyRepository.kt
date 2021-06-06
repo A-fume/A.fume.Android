9f5cf7b9c509cfdc04d9e833dabc6a922393ee93
Haeeul <etoilune00@gmail.com>
2021-06-06 13:24:12 +0900

    [refactor] #47 Modify survey activity and add user token

--- app/src/main/java/com/afume/afume_android/data/repository/SurveyRepository.kt
+++ app/src/main/java/com/afume/afume_android/data/repository/SurveyRepository.kt
@@ -1,16 +1,16 @@
 package com.afume.afume_android.data.repository
 
+import com.afume.afume_android.AfumeApplication
 import com.afume.afume_android.data.remote.RemoteDataSource
 import com.afume.afume_android.data.remote.RemoteDataSourceImpl
 import com.afume.afume_android.data.vo.request.RequestSurvey
-import com.afume.afume_android.util.getToken
 
 class SurveyRepository {
     val remoteDataSource: RemoteDataSource = RemoteDataSourceImpl()
 
     suspend fun getSeries() = remoteDataSource.getSeries()
     suspend fun getSurveyPerfume()=remoteDataSource.getSurveyPerfume(
-        getToken()
+        AfumeApplication.prefManager.accessToken
     )
     suspend fun getKeyword()=remoteDataSource.getKeyword()
     suspend fun postSurvey(token: String, body: RequestSurvey)=remoteDataSource.postSurvey(token, body)
