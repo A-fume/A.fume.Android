a06006ae931d8024e49690174bbf43dc0f0354f4
EuneeChung <chungee375@gmail.com>
2021-06-06 22:10:50 +0900

    [feature] #45 Send perfume info to Note Activity
    When click wish list, send perfume info

--- app/src/main/java/com/afume/afume_android/ui/my/wishlist/WishListAdapter.kt
+++ app/src/main/java/com/afume/afume_android/ui/my/wishlist/WishListAdapter.kt
@@ -5,28 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.afume.afume_android.data.vo.ParcelableWishList
 import com.afume.afume_android.data.vo.response.PerfumeInfo
 import com.afume.afume_android.databinding.RvItemMyWishlistBinding
 import com.afume.afume_android.ui.note.NoteActivity
 
 
-class WishListAdapter() : RecyclerView.Adapter<WishListRecyclerHolder>() {
+class WishListAdapter (): RecyclerView.Adapter<WishListRecyclerHolder>(){
     var data = mutableListOf<PerfumeInfo>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WishListRecyclerHolder {
-        val binding =
-            RvItemMyWishlistBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding=RvItemMyWishlistBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return WishListRecyclerHolder(binding)
     }
 
-    override fun getItemCount(): Int = data.size
+    override fun getItemCount(): Int =data.size
 
     override fun onBindViewHolder(holder: WishListRecyclerHolder, position: Int) {
         holder.bind(data[position])
     }
 
-    internal fun setWishListData(data: MutableList<PerfumeInfo>?) {
-        if (data != null) this.data = data
+    internal fun setWishListData(data: MutableList<PerfumeInfo>?){
+        if(data!=null) this.data=data
         notifyDataSetChanged()
     }
 
@@ -36,13 +36,14 @@ class WishListRecyclerHolder(val binding: RvItemMyWishlistBinding) : RecyclerVie
     fun bind(data: PerfumeInfo) {
         binding.wishlist = data
         binding.root.setOnClickListener {
-            goToNotePageWithPerfumeIdx(it, data.perfumeIdx)
+            goToNotePageWithPerfumeInfo(it, data)
         }
     }
 
-    fun goToNotePageWithPerfumeIdx(view: View, perfumeIdx: Int) {
+    fun goToNotePageWithPerfumeInfo(view: View, perfumeInfo: PerfumeInfo) {
         val intent = Intent(view.context, NoteActivity::class.java)
-        intent.putExtra("perfumeIdx", perfumeIdx)
+        val wishListPerfume = ParcelableWishList(perfumeInfo.perfumeIdx,perfumeInfo.name,perfumeInfo.brandName,perfumeInfo.imageUrl)
+        intent.putExtra("wishListPerfume", wishListPerfume)
         view.context.startActivity(intent)
     }
 
