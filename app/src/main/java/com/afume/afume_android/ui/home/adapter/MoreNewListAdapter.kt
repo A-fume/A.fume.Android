434f9be5d66591fb232d0880b9307e37ee840d65
Haeeul <etoilune00@gmail.com>
2021-06-08 20:28:09 +0900

    [feat] #30 Add intent to detail page

--- app/src/main/java/com/afume/afume_android/ui/home/adapter/MoreNewListAdapter.kt
+++ app/src/main/java/com/afume/afume_android/ui/home/adapter/MoreNewListAdapter.kt
@@ -1,13 +1,16 @@
 package com.afume.afume_android.ui.home.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.afume.afume_android.R
 import com.afume.afume_android.data.vo.response.NewPerfumeItem
 import com.afume.afume_android.databinding.RvItemHomeMoreNewBinding
+import com.afume.afume_android.ui.detail.PerfumeDetailActivity
 
 class MoreNewListAdapter(private val context: Context) : RecyclerView.Adapter<MoreNewListViewHolder>() {
     var data = mutableListOf<NewPerfumeItem>()
@@ -42,5 +45,15 @@ class MoreNewListAdapter(private val context: Context) : RecyclerView.Adapter<Mo
 class MoreNewListViewHolder(val binding:RvItemHomeMoreNewBinding) : RecyclerView.ViewHolder(binding.root){
     fun bind(item : NewPerfumeItem){
         binding.item = item
+
+        binding.root.setOnClickListener {
+            onClickPerfume(it, item.perfumeIdx)
+        }
+    }
+
+    private fun onClickPerfume(view: View, perfumeIdx: Int){
+        val intent = Intent(view.context, PerfumeDetailActivity::class.java)
+        intent.putExtra("perfumeIdx", perfumeIdx)
+        view.context.startActivity(intent)
     }
 }
