1bed3175615ee7a63f4f29de49378f315cffce27
Haeeul <etoilune00@gmail.com>
2021-10-08 01:15:53 +0900

    [refactor] #90 Modify gender value in home view

--- app/src/main/java/com/afume/afume_android/data/vo/response/ResponseLogin.kt
+++ app/src/main/java/com/afume/afume_android/data/vo/response/ResponseLogin.kt
@@ -3,7 +3,7 @@ package com.afume.afume_android.data.vo.response
 data class ResponseLogin(
     val userIdx : Int,
     val nickname : String,
-    val gender : String,
+    val gender : Int,
     val birth : Int,
     val token : String,
     val refreshToken : String
