37e16da4c1b67013a9d479056b777066fbf09998
Haeeul <etoilune00@gmail.com>
2022-02-09 23:28:35 +0900

    [refactor] #93 Modify seek bar at note view

--- app/src/main/java/com/afume/afume_android/util/SeekBar.kt
+++ app/src/main/java/com/afume/afume_android/util/SeekBar.kt
@@ -7,7 +7,7 @@ import androidx.core.content.ContextCompat
 import com.afume.afume_android.R
 import com.afume.afume_android.ui.note.NoteViewModel
 
-private fun setSelectedSeekBarTxtBold(list: List<TextView>, select: Int){
+fun setSelectedSeekBarTxtBold(list: List<TextView>, select: Int){
     for(i in list.indices){
         if(i==select){
             list[i].setSelectedSeekBarTxt(true)
@@ -17,20 +17,24 @@ private fun setSelectedSeekBarTxtBold(list: List<TextView>, select: Int){
     }
 }
 
+private fun setSeekBarProgress(vm : NoteViewModel, type: String, progress: Int){
+    when (type){
+        "longevity" -> {
+            vm.longevityProgress.value = progress
+        }
+        "reverb" -> {
+            vm.reverbProgress.value = progress
+        }
+        "gender" -> {
+            vm.genderProgress.value = progress
+        }
+    }
+}
+
 class SeekBarListener(val context: Context, val seekBar: SeekBar, val list: List<TextView>, val vm : NoteViewModel, val type: String) : SeekBar.OnSeekBarChangeListener{
     override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
         setSelectedSeekBarTxtBold(list,progress)
-        when (type){
-            "longevity" -> {
-                vm.longevityProgress.value = progress
-            }
-            "reverb" -> {
-                vm.reverbProgress.value = progress
-            }
-            "gender" -> {
-                vm.genderProgress.value = progress
-            }
-        }
+        setSeekBarProgress(vm,type,progress)
 
     }
 
@@ -39,6 +43,7 @@ class SeekBarListener(val context: Context, val seekBar: SeekBar, val list: List
         if(seekBar.progress==0){
             setSelectedSeekBarTxtBold(list,0)
         }
+        setSeekBarProgress(vm,type,0)
     }
 
     override fun onStopTrackingTouch(p0: SeekBar?) {
