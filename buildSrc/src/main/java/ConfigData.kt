7510b572c538d0dc756512b049be3f74cbede2f9
hee.youn <heesung6701@naver.com>
2022-08-11 23:39:23 +0900

    gradle 7.0.2로 올리고 compile SDK 31로 upgrade

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -1,13 +1,13 @@
 object ConfigData {
-    const val compileSdkVersion = 30
-    const val buildToolsVersion = "30.0.0"
+    const val compileSdkVersion = 31
+    const val buildToolsVersion = "31.0.0"
     const val minSdkVersion = 23
-    const val targetSdkVersion = 30
+    const val targetSdkVersion = 31
     const val versionCode = 1
     const val versionName = "1.0"
 
     // gradle
-    const val gradleVersion = "4.1.1"
+    const val gradleVersion = "7.0.2"
     const val gradlePluginVersion = "1.5.31"
 
     // kotlin
