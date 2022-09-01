1d12ddb38a97a70740775a9d292cf1a799dec4f2
hee.youn <heesung6701@naver.com>
2022-09-01 22:35:40 +0900

    upgrade compileSDK, targetSDK to 33 upgrade gradle 7.0.2 to 7.3.3

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -1,15 +1,16 @@
 object ConfigData {
-    const val compileSdkVersion = 31
-    const val buildToolsVersion = "31.0.0"
+    const val compileSdkVersion = 33
+    const val buildToolsVersion = "33.0.0"
     const val minSdkVersion = 23
-    const val targetSdkVersion = 31
+    const val targetSdkVersion = 33
     const val versionCode = 1
     const val versionName = "1.0"
 
     // gradle
-    const val gradleVersion = "7.0.2"
-    const val gradlePluginVersion = "1.5.31"
+    const val gradleVersion = "7.3.3"
+    const val agpVersion = "7.2.1"
+    const val gradlePluginVersion = "1.7.0"
 
     // kotlin
-    const val kotlin_version = "1.4.20"
+    const val kotlin_version = "1.7.0"
 }
