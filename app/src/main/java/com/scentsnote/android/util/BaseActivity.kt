0cd50316f4ca6f69ead644a4b064a4a512de9e75
Haeeul <etoilune00@gmail.com>
2022-10-08 13:36:46 +0900

    [refactor] #223 Add base fragment

--- app/src/main/java/com/scentsnote/android/util/BaseActivity.kt
+++ app/src/main/java/com/scentsnote/android/util/BaseActivity.kt
@@ -1,9 +1,6 @@
 package com.scentsnote.android.util
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -19,14 +16,14 @@ abstract class BaseActivity<T: ViewDataBinding>(@LayoutRes val layoutRes: Int): 
         binding.lifecycleOwner = this@BaseActivity
 
         initView()
-        initActionView()
-        initLiveData()
+        initListener()
+        initObserver()
     }
 
 //    open fun beforeSetContentView() {}
     open fun initView() {}
-    open fun initActionView() {}
-    open fun initLiveData() {}
+    open fun initListener() {}
+    open fun initObserver() {}
 //    open fun afterOnCreate() {}
 
 }
