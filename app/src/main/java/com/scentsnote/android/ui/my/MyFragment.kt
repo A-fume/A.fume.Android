7d5327c642165b6884d8386a5793f19e582f4875
Haeeul <etoilune00@gmail.com>
2022-10-18 23:53:41 +0900

    [refactor] #229 Modify navigation drawer in my tab

--- app/src/main/java/com/scentsnote/android/ui/my/MyFragment.kt
+++ app/src/main/java/com/scentsnote/android/ui/my/MyFragment.kt
@@ -116,9 +116,9 @@ class MyFragment : Fragment() {
         else {
             binding.myNavigationDrawer.menu.removeGroup(R.id.group_my_info)
             binding.myNavigationDrawer.menu.removeGroup(R.id.group_password)
+            binding.myNavigationDrawer.menu.removeGroup(R.id.group_tip_off)
             binding.myNavigationDrawer.menu.removeGroup(R.id.group_feedback)
             binding.myNavigationDrawer.menu.removeGroup(R.id.group_logout)
-            binding.myNavigationDrawer.menu.removeGroup(R.id.group_withdrawal)
             binding.myNavigationDrawer.menu.removeGroup(R.id.group_divider)
 
             binding.myNavigationDrawer.inflateMenu(R.menu.navigation_drawer_login)
@@ -155,7 +155,7 @@ class MyFragment : Fragment() {
                     if(menuItem.itemId == R.id.feedback){
                         intent.putExtra("url", "feedback")
                     }else{
-                        intent.putExtra("url", "withdrawal")
+                        intent.putExtra("url", "tipOff")
                     }
                     startActivity(intent)
                 }
