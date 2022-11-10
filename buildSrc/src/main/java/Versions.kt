337ad01810e1fc88d3b316153f89db9716b9ef2f
Haeeul <etoilune00@gmail.com>
2022-11-11 00:08:42 +0900

    [refactor] #236 Add the ability to encrypt user information

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -70,4 +70,7 @@ object Versions {
     // firebase
     const val gms = "4.3.13"
     const val firebase = "30.4.1"
+
+    // encryptedSharedPreferences
+    const val encryptedSharedPreferences = "1.1.0-alpha03"
 }
