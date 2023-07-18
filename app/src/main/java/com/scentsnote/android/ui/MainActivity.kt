50d9ef84e6a7f0d0a4f025f4cfe83cc4d85efaab
Haeeul <etoilune00@gmail.com>
2023-07-18 13:25:42 +0900

    [fix] #284 뒤로가기 이벤트 버그 수정

--- app/src/main/java/com/scentsnote/android/ui/MainActivity.kt
+++ app/src/main/java/com/scentsnote/android/ui/MainActivity.kt
@@ -9,9 +9,6 @@ import com.scentsnote.android.R
 import com.scentsnote.android.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.scentsnote.android.ScentsNoteApplication.Companion.firebaseAnalytics
-import com.scentsnote.android.ui.home.HomeFragment
-import com.scentsnote.android.ui.my.MyFragment
-import com.scentsnote.android.ui.search.SearchFragment
 import com.scentsnote.android.utils.base.BaseActivity
 import com.scentsnote.android.utils.extension.setClickEvent
 
@@ -45,34 +42,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private fun initNavigationEvent(){
         bottomNav.setOnItemSelectedListener { menuItem ->
             var buttonName = ""
-            var fragmentName = ""
 
             when (menuItem.itemId) {
                 R.id.homeFragment -> {
                     buttonName = "NavigationHome"
-                    fragmentName = HomeFragment::class.java.name
                 }
                 R.id.searchFragment -> {
                     buttonName = "NavigationSearch"
-                    fragmentName = SearchFragment::class.java.name
                 }
                 R.id.myPageFragment -> {
                     buttonName = "NavigationMy"
-                    fragmentName = MyFragment::class.java.name
                 }
             }
 
             firebaseAnalytics.setClickEvent(buttonName)
 
-            changeFragment(fragmentName)
-
+            navController.navigate(menuItem.itemId)
             true
         }
-    }
-
-    private fun changeFragment(className: String) {
-        val fragment = supportFragmentManager.fragmentFactory.instantiate(classLoader, className)
-        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
     }
 
     override fun onBackPressed() {
