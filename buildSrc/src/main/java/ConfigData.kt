1b8e3cc3683ef67e3042da1f27279d5265860da1
Haeeul <etoilune00@gmail.com>
2023-09-21 07:32:28 +0900

    1.5.1 세팅

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -3,8 +3,8 @@ object ConfigData {
     const val buildToolsVersion = "30.0.3"
     const val minSdkVersion = 23
     const val targetSdkVersion = 31
-    const val versionCode = 9
-    const val versionName = "1.5.0"
+    const val versionCode = 11
+    const val versionName = "1.5.1"
 
     // gradle
     const val gradleVersion = "7.3.1"
